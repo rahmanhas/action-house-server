268ad7c4bfe106f681566e31d1fc4ef0bb80283e
rahmanhas <rahmanhas@gmail.com>
2023-05-20 23:39:38 +0600

    some bug fixing

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-
-
 app.use(cors())
 app.use(express.json())
-require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.di1kiaj.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,20 +28,27 @@ async function run() {
     const galleryCollection = client.db('actionHouse').collection('gallery-images');
 
     //indexing
-    const indexKeys = { toyName: 1, subCategory: 1 };
-    const indexOptions = { name: "titleCategory" }
+    //const indexKeys = { toyName: 1, subCategory: 1 };
+    //const indexOptions = { name: "titleCategory" }
 
-    const result = await toyCollection.createIndex(indexKeys, indexOptions)
+    //const result = await toyCollection.createIndex(indexKeys, indexOptions)
 
+    // app.get("/searchbytoyname/:toyname", async (req, res) => {
+    //   const searchedToyName = req.params.toyname;
+
+    //   const result = await toyCollection.find({
+    //     $or: [
+    //       { toyName: { $regex: searchedToyName, $options: "i" } },
+    //       // {subCategory: {$regex:searchedToyName,$options:"i"}}
+    //     ]
+    //   }).toArray()
+    //   res.send(result)
+    // }){ $regex: req.params.searchedToyName, $options: "i" }
+    //{ toyName: { $regex: searchedToyName, $options: "i" } }
     app.get("/searchbytoyname/:toyname", async (req, res) => {
       const searchedToyName = req.params.toyname;
 
-      const result = await toyCollection.find({
-        $or: [
-          { toyName: { $regex: searchedToyName, $options: "i" } },
-          // {subCategory: {$regex:searchedToyName,$options:"i"}}
-        ]
-      }).toArray()
+      const result = await toyCollection.find({ toyName: { $regex: searchedToyName, $options: "i" } }).toArray()
       res.send(result)
     })
 
@@ -55,17 +60,17 @@ async function run() {
       const result = await toyCollection.insertOne(toy);
       res.send(result)
     })
-    app.get('/storedtoydata', async (req, res) => {
+    app.get('/alldata', async (req, res) => {
       const cursor = toyCollection.find().limit(20);
-      const result = await cursor.toArray(); 
+      const result = await cursor.toArray();
       res.send(result);
     })
     //Home Page shop by category sorted
-    app.get('/storedtoydata/:subCategory', async (req, res) => {
+    app.get('/findsubcategory/:subCategory', async (req, res) => {
       const result = await toyCollection.find({ subCategory: req.params.subCategory }).limit(4).toArray();
       res.send(result)
     })
-    app.get('/storedtoydata/:id', async (req, res) => {
+    app.get('/findtoyid/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await toyCollection.findOne(query)
@@ -133,7 +138,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
